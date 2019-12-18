c5fe35d282eb85dc7959bce6a866cadac0f3eee9
Ashley Claiborne <languageisaliquid@gmail.com>
2019-12-17 21:20:50 -0600

    team component prints

---
+++ src/components/Player/Player.js
@@ -0,0 +1,10 @@
+import './Player.scss';
+import React from 'react';
+
+class Player extends React.Component {
+  render() {
+    return (<div>Player div</div>);
+  }
+}
+
+export default Player;
---
+++ src/components/Team/Team.js
@@ -0,0 +1,35 @@
+import './Team.scss';
+import React from 'react';
+import playerData from '../../helpers/data/playerData';
+import authData from '../../helpers/data/authData';
+import Player from '../Player/Player';
+
+class Team extends React.Component {
+  state = {
+    players: [],
+  }
+
+  getPlayers = () => {
+    playerData.getPlayersByUid(authData.getUid())
+      .then((players) => {
+        this.setState({ players });
+      }).catch((err) => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getPlayers();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My Team</h1>
+        <div className="row d-flex flex-wrap justify-content-around">
+          {this.state.players.map((player) => <Player key={player.id} player={player} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Team;
