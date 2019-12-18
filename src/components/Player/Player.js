5b8490c139cc68cdafc6c6a270d7ba128dea0c92
Ashley Claiborne <languageisaliquid@gmail.com>
2019-12-17 21:37:22 -0600

    players print

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,9 +1,25 @@
 import './Player.scss';
 import React from 'react';
+import playerShape from '../../helpers/propz/playerShape';
 
 class Player extends React.Component {
+  static propTypes = {
+    player: playerShape.playerShape,
+  }
+
   render() {
-    return (<div>Player div</div>);
+    const { player } = this.props;
+
+    return (
+      <div className="card col-3 m-2">
+        <img src={player.imageUrl} class="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{player.name}</h5>
+          <p className="card-text">{player.position}</p>
+          <button className="btn btn-warning">Remove</button>
+        </div>
+      </div>
+    );
   }
 }
 
