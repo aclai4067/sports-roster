448671eec169bcc0a5fc87f37b2b510cce2056f6
Ashley Claiborne <languageisaliquid@gmail.com>
2019-12-27 22:55:02 -0600

    read complete

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -12,7 +12,7 @@ class Player extends React.Component {
 
     return (
       <div className="card col-3 m-2">
-        <img src={player.imageUrl} class="card-img-top" alt="..." />
+        <img src={player.imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{player.name}</h5>
           <p className="card-text">{player.position}</p>
